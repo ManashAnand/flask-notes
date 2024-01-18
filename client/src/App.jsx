fc4e5a712670202bad51b403790234f3cc58208a
MANASH <anandmanash321@gmail.com>
2024-01-19 00:33:07 +0530

    added resize route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
 
-import axios from 'axios'
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Home from './components/Home';
 
 function App() {
 
-  const handleBtn = async () => {
-    const res = await axios.get('http://localhost:5000/');
-    console.log(res)
-  }
-  const handleBtn2 = async () => {
-    const name="manash"
-    const res = await axios.get('http://localhost:5000/'+name);
-    console.log(res)
-  }
 
   return (
     <>
-        <button onClick={handleBtn}>getData</button>
-        <button onClick={handleBtn2}>getDataVariable</button>
+    <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+    </Routes>
+
+
+     
     </>
   )
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import axios from "axios";
+import { useState } from "react";
+const Home = () => {
+  const [img, setImg] = useState();
+ 
+  const sendImg = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", img);
+    const res = await axios.post("http://localhost:5000/grayscale", formData);
+    console.log(res);
+  };
+
+  const newRoute = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", img);
+    const res = await axios.post("http://localhost:5000/newroute", formData);
+    console.log(res);
+  }
+  return (
+    <>
+        
+    <div className=" flex justify-center items-center mt-10">
+      <div class="shrink-0">
+        <img
+          class="h-16 w-16 object-cover rounded-full"
+          src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80"
+          alt="Current profile photo"
+        />
+      </div>
+      <label class="block">
+        <span class="sr-only">Choose profile photo</span>
+        <input
+          type="file"
+          onChange={(e) => setImg(e.target.files[0])}
+          class="block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100
+    "
+        />
+      </label>
+    </div>
+    <div className=" flex justify-center items-center mt-10">
+        <div className="py-2.5 px-5 border rounded-xl bg-violet-700 text-white text-2xl cursor-pointer active:scale-95" onClick={sendImg}>Change image to grayscale</div>
+    </div>
+    <div className=" flex justify-center items-center mt-10">
+        <div className="py-2.5 px-5 border rounded-xl bg-violet-700 text-white text-2xl cursor-pointer active:scale-95" onClick={newRoute}>Change image to 256 and 256</div>
+    </div>
+    </>
+  );
+};
+
+export default Home;
